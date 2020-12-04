05784ac0bd9f5cd7626dd10d0f247560665409c8
najimali <najim.ali32@gmail.com>
2020-12-04 21:30:23 +0530

    Login Page Done

---
+++ client/src/hooks/useLocalStorage.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+// We are creating a local storage so that our Id persistate
+//@params:
+// key- It is going to what we stire inside a local storage
+// initialValue - It is what we are passing to useState
+
+const PREFIX = "chatapp-react-";
+function useLocalStorage(key, initialValue) {
+  // PREFIX is add to avoid conflict from different Projects.
+  const prefixedKey = PREFIX + key;
+  // Code to get value from local storage & put into our state
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+    if (typeof initialValue === "function") return initialValue();
+    else return initialValue;
+  });
+
+  // Anytime our value changed or prefixedKey changed then we reput our value into localStorage
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
